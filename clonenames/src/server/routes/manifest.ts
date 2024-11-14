a239695423de540a245e38a90570d0e9ea7dc987
alexhoff2 <unalexhoff@gmail.com>
2024-11-14 14:24:31 -0800

    Updated index imports for simplification purposes, deleted unnecessary imports from original manifest file.

--- clonenames/src/server/routes/manifest.ts
+++ clonenames/src/server/routes/manifest.ts
@@ -2,4 +2,3 @@ export { default as auth } from "./auth";
 export { default as games } from "./games";
 export { default as home } from "./root";
 export { default as mainLobby } from "./games";
-export { default as test } from "./test";
