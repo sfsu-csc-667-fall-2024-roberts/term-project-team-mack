f5b3ba2e7c28c5302905d664bf18cf8e0a1c6630
mar10fam <mphamrtin@gmail.com>
2024-12-07 21:52:32 -0800

    migrations for db, fixed live reload

---
+++ clonenames/migrations/1733352934168_create-db.ts
@@ -0,0 +1,114 @@
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    // create users table
+    pgm.createTable("users", {
+        id: "id",
+        email: {
+            type: "varchar(255)",
+            unique: true,
+            notNull: true
+        },
+        username: {
+            type: "varchar(255)",
+            unique: true,
+            notNull: true
+        },
+        password: {
+            type: "varchar(255)",
+            notNull: true
+        },
+        created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    });
+
+    // create games table
+    pgm.createTable("games", {
+        id: "id",
+        name: {
+            type: "varchar(255)",
+            notNull: true
+        },
+        status: {
+            type: "varchar(15)",
+            notNull: true
+        },
+        created_by: {
+            type: "integer",
+            references: "users(id)",
+            onDelete: "CASCADE"
+        },
+        created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        },
+        updated_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    });
+
+    // create GamePlayers table
+    pgm.createTable("GamePlayers", {
+        id: "id",
+        game_id: {
+            type: "integer",
+            references: "games(id)",
+            onDelete: "CASCADE"
+        },
+        user_id: {
+            type: "integer",
+            references: "users(id)",
+            onDelete: "CASCADE"
+        },
+        role: {
+            type: "varchar(10)",
+            notNull: true
+        },
+        team: {
+            type: "varchar(5)",
+            notNull: true
+        },
+        joined_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    });
+
+    // create GameState table
+    pgm.createTable("GameState", {
+        id: "id",
+        game_id: {
+            type: "integer",
+            references: "games(id)",
+            onDelete: "CASCADE"
+        },
+        grid: {
+            type: "jsonb",
+            notNull: true
+        },
+        current_turn: {
+            type: "varchar(5)",
+            notNull: true
+        },
+        created_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        },
+        updated_at: {
+            type: "timestamp",
+            default: pgm.func("current_timestamp")
+        }
+    });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropTable("GameState");
+    pgm.dropTable("GamePlayers");
+    pgm.dropTable("games");
+    pgm.dropTable("users");
+}
--- clonenames/src/server/routes/test.ts
+++ clonenames/src/server/routes/test.ts
@@ -4,13 +4,18 @@ import db from "../db/connection";
 const router = express.Router();
 
 router.get("/", async (_request, response) => {
-    await db.any(`INSERT INTO test ("test_string") VALUES ($1)`, [
-        `Helo on ${new Date().toLocaleString()}`
-    ]);
+    try {
+        await db.any(`INSERT INTO test ("test_string") VALUES ($1)`, [
+            `Hello on ${new Date().toLocaleString()}`
+        ]);
 
-    const results = await db.any(`SELECT * FROM test`);
+        const results = await db.any(`SELECT * FROM test`);
 
-    response.json(results);
+        response.json(results);
+    } catch(err) {
+        // @ts-expect-error TODO: Fix this typing so I can use any in catch statements
+        response.status(500).json({ error: err.message });
+    }
 });
 
 export default router;
