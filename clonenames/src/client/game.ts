d86f8839668b422e00d0d493e541f7412a28278c
Bryan-Rodriguez-ldnbm <bryanRod01@outlook.com>
2024-12-20 02:24:30 -0800

    Client side script for an async function to fetch gameboard data

---
+++ clonenames/src/client/game.ts
@@ -0,0 +1,27 @@
+// made async for promise function
+(async () => {
+    const gameBoardElement = document.getElementById("gameBoard")! as HTMLDivElement;
+
+    fetch("/games/setGameboard", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then(response => {
+        if(response.status !== 200) {
+            console.error("Error: ", response);
+        }
+        return response.json(); // Parse the JSON response
+        }).then((gameboard: { code: string; colorCode: string }[]) => {
+            const gameBoardElement = document.getElementById("gameBoard")! as HTMLDivElement;
+    
+            gameboard.forEach(({ code, colorCode }) => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+                card.style.border = `2px solid ${colorCode}`;
+                card.textContent = code;
+    
+                gameBoardElement.appendChild(card);
+            });
+        })
+})();
